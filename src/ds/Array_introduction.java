27fa3aaac1a4650f23310432c9c27f77dd72616a
Sanyam Aggarwal <52964101+sanyam-aggarwal@users.noreply.github.com>
2020-07-15 14:23:28 +0530

    Update Array_introduction.java

--- src/ds/Array_introduction.java
+++ src/ds/Array_introduction.java
@@ -1,10 +1,18 @@
 package ds;
-
+import java.util.*;
 public class Array_introduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 Scanner scan = new Scanner(System.in);
+	        int n = scan.nextInt();
+	    int a[]=new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        a[i]=scan.nextInt();
+	    }
+	    for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
 	}
 
 }
