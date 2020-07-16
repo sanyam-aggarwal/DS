2e6d43ae53b3d2710784a08e54f5c7fe234cc034
Sanyam Aggarwal <52964101+sanyam-aggarwal@users.noreply.github.com>
2020-07-16 13:23:16 +0530

    Update OptimizedBubbleSort.java

--- src/ds/OptimizedBubbleSort.java
+++ src/ds/OptimizedBubbleSort.java
@@ -1,10 +1,29 @@
 package ds;
+import java.util.*;
 
-public class OptimizedBubbleSort {
+public class OptimizedBubbleSort  {
 
-	public static void main(String[] args) {
-		
-
-	}
-
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(),a[]=new int[n];
+        boolean swapped;
+        for (int i = 0; i < n; i++)  a[i]=scan.nextInt();
+        scan.close();
+        int c,d;
+        System.out.println("Bubble sort gives:");
+        for(c=0;c<n-1;c++){
+            swapped=false;
+            for(d=0;d<n-c-1;d++) if(a[d]>a[d+1])
+            {
+            a[d]=a[d]+a[d+1];
+            a[d+1]=a[d]-a[d+1];
+            a[d]=a[d]-a[d+1];
+            swapped = true; 
+            }
+            if(swapped==false) break;
+        }
+        for (int i = 0; i < a.length; i++) System.out.print(a[i]);
+        
+    }
 }
