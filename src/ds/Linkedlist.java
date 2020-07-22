e2bcd773a743d1c2ca399079332f57a36085aa26
Sanyam Aggarwal <52964101+sanyam-aggarwal@users.noreply.github.com>
2020-07-22 22:13:33 +0530

    Update Linkedlist.java
    add element at front, insert between & at end of a linked list

--- src/ds/Linkedlist.java
+++ src/ds/Linkedlist.java
@@ -1,59 +1,79 @@
 package ds;
 public class Linkedlist
 {
-	Node head;
-	void add(int data)
-	{
-		Node toAdd=new Node(data);
-		
-		if(isEmpty())
-		{
-			head=toAdd;
-			return;
-		}
-		
-		Node temp=head;
-		
-		while(temp.next != null)
-		{
-			temp=temp.next;
-		}
-		temp.next=toAdd;
-	}
-	void print()
-	{
-Node temp=head;
-		
-		while(temp != null)
-		{
-			System.out.print(temp.data + " ");
-			temp=temp.next;
-		}
-	}
-	boolean isEmpty()
+    Node head;
+    void push(int data)
+    {
+        Node newnode=new Node(data);
+        newnode.next=head;
+        head=newnode;
+    }
+    
+    void insert(Node prevnode,int data)
+    {
+        if(prevnode==null)
+        {
+            System.out.print("the given previous note cannot be null");
+            return;
+        }
+        Node newnode=new Node(data);
+        newnode.next=prevnode.next;
+        prevnode.next=newnode;
+    }
+    
+    void add(int data)
+    {
+        Node toAdd=new Node(data);
+        if(isEmpty())
+        {
+            head=toAdd;
+            return;
+        }
+        else
+        {
+            Node last=head;
+            while(last.next!=null)
+            {
+                last=last.next;    
+            }
+            last.next=toAdd;
+        }
+    }
+   
+     void print()
+     {
+         Node last=head;
+            while(last!=null)
+            {
+                System.out.print(last.data +" ");
+                last=last.next;
+            } 
+     }
+      boolean isEmpty()
 	{
 		return head == null;
 	}
-	static class Node
+    static class Node
+    {
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data=data;
+            next=null;
+        }
+    }
+	public static void main(String[] args) 
 	{
-		int data;
-		Node next;
-		public Node(int d)
-		{
-			data=d;
-			next=null;
-		}
-	}
-	
-	public static void main(String[] args)
-	{
-		Linkedlist myll=new Linkedlist();
-		myll.add(52);
-		myll.add(12);
-		myll.add(56);
-		myll.add(45);
-		myll.add(78);
-		myll.print();
-		
+	    Linkedlist myll=new Linkedlist();
+	    myll.add(52);
+	    myll.add(2);
+	    myll.add(42);
+	    myll.add(32);
+	    myll.add(22);
+	    myll.push(12);
+	    myll.insert(myll.head.next,8);
+	    myll.print();
+	    
 	}
 }
